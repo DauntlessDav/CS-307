23535e9cacba0e47cacd8f288957709023093ef2
DauntlessDav <abramsda@outlook.com>
2024-10-11 21:34:31 -0700

    id lookup

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -34,13 +34,16 @@ const users = {
   ]
 };
 
+app.use(express.json());
+
 const findUserByName = (name) => {
   return users["users_list"].filter(
     (user) => user["name"].toLowerCase() === name
   );
 };
 
-app.use(express.json());
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
 
 app.get("/users", (req, res) => {
   const name = req.query.name;
@@ -51,6 +54,16 @@ app.get("/users", (req, res) => {
     res.send(result);
   } else {
     res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
   }
 });
 
